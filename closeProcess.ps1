541b0d1836698701f075d1d3e8b43968fcc60f9e
name <willspri@gmail.com>
2025-08-31 15:12:13 +1200

    updated

--- closeProcess.ps1
+++ closeProcess.ps1
@@ -1,8 +1,8 @@
 while ($true) {
+	#change notepad to desired program
     $process = Get-Process -Name "notepad" -ErrorAction SilentlyContinue
     if ($process) {
         Stop-Process -Id $process.Id -Force
-        Write-Output "Notepad was running and has been stopped."
     }
 
     # needs to sleep for short time
